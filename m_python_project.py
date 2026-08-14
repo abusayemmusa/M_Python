cd51e1b2d808b4cf1710176a166d49589d502f9d
Md Abu Sayem Musa <143425981+abusayemmusa@users.noreply.github.com>
2026-08-14 18:08:47 +0600

    Update m_python_project.py

--- m_python_project.py
+++ m_python_project.py
@@ -1,5 +1,5 @@
 ========================================================================================================
-# Hello World project
+1. Hello World project
 ========================================================================================================
 def print_hello():
     name = input("Enter Your Name : ")
@@ -8,5 +8,22 @@ def print_hello():
 if __name__ == "__main__":
     print_hello()
 ========================================================================================================
+1. Calculator
+========================================================================================================
+option = input("please enter add, sub, mul div: ")
+user1 = int(input("enter the first number:"))
+user2 = int(input("enter the first number:"))
 
+if option == "add":
+    print(f"The sum of {user1} and {user2} is -{user1 + user2}")
+elif option == "sub":
+    print(f"The difference between {user1} and {user2} is -{user1 + user2}")
+elif option == "mul":
+    print(f"The product of {user1} and {user2} is -{user1 + user2}")
+elif option == "div":
+    print(f"The quotient of {user1} and {user2} is -{user1 + user2}")
+else:
+    print("invalid option. Please enter add, sub, mul, or div.")
+========================================================================================================
+3. Even/Odd Checker
 ========================================================================================================
